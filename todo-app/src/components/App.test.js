47ca96844932546fd00f07f1f7b789b753d0d6c2
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-02-11 23:55:50 -0500

    Adding snapshot tests

--- todo-app/src/components/App.test.js
+++ todo-app/src/components/App.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 jest.mock('../containers/AddTodo', () => () => 'AddTodo');
 jest.mock('../containers/VisibleTodoList', () => () => 'VisibleTodoList');
 jest.mock('../containers/FilterLink', () => () => 'FilterLink');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App basic tests', () => {    
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+
+describe('App snapshot tests', () => {    
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
