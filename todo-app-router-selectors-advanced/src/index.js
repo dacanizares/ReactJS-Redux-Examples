365103d209924e5c3bd9cc567962153ee78cf8f4
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-03-16 11:01:36 -0500

    Adding support for Redux Dev Tools extension

--- todo-app-router-selectors-advanced/src/index.js
+++ todo-app-router-selectors-advanced/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import todoApp from './reducers'
 import Root from './components/Root'
 import { loadState, saveState } from './localStorage'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const persistedState = loadState()
 const store = createStore(
     todoApp,
-    persistedState
+    persistedState,
+    composeEnhancers(applyMiddleware())
 )
 console.log(persistedState)
 
